998ce9ab84b817bc203f0140581a3e46ff12da38
zaineli <zali.bscs22seecs@seecs.edu.pk>
2024-10-13 04:54:39 +0500

    bs4 added + Cors + Search Endpoint added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+def scrape_search_results(query):
+    url = f"https://www.google.com/search?q={query}"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = []
+
+    for item in soup.find_all('h3'):
+        parent = item.find_parent('a')
+        if parent:
+            links.append(parent['href'])
+
+    return links[:10]
+
+@app.get("/search")
+def search(query: str):
+    results = scrape_search_results(query)
+    results = [ link.replace('/url?q=', '') for link in results]
+    return results
